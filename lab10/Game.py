ba97eceaef1e6784f3388f69dc917e1c62c2e4ee
Daiwans <116733031+Daiwans@users.noreply.github.com>
2023-12-15 19:12:38 +0600

    lab10
    lab10

---
+++ lab10/Game.py
@@ -0,0 +1,109 @@
+import pygame, sys, random, time
+
+# Initialize Pygame
+pygame.init()
+
+# Setting up FPS
+FPS = 60
+FramePerSec = pygame.time.Clock()
+
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+# Screen size
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 600
+
+# Player and Enemy speed
+SPEED = 5
+SCORE = 0
+
+# Fonts
+font = pygame.font.SysFont("Verdana", 60)
+font_small = pygame.font.SysFont("Verdana", 20)
+game_over = font.render("Game Over", True, WHITE)
+
+# Load images (using original file names)
+background = pygame.image.load("AnimatedStreet.png")
+
+# Create a screen
+DISPLAYSURF = pygame.display.set_mode((400, 600))
+pygame.display.set_caption("Space Shooter")
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("Enemy.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
+
+    def move(self):
+        global SCORE
+        self.rect.move_ip(0, SPEED)
+        if self.rect.top > SCREEN_HEIGHT:
+            SCORE += 1
+            self.rect.bottom = 0
+            self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("Player.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (160, 520)
+
+    def move(self):
+        pressed_keys = pygame.key.get_pressed()
+        if self.rect.left > 0 and pressed_keys[pygame.K_LEFT]:
+            self.rect.move_ip(-5, 0)
+        if self.rect.right < SCREEN_WIDTH and pressed_keys[pygame.K_RIGHT]:
+            self.rect.move_ip(5, 0)
+
+# Creating Sprites
+P1 = Player()
+E1 = Enemy()
+
+# Creating Sprite Groups
+enemies = pygame.sprite.Group()
+enemies.add(E1)
+all_sprites = pygame.sprite.Group()
+all_sprites.add(P1, E1)
+
+# Adding a new User event for speed increase
+INC_SPEED = pygame.USEREVENT + 1
+pygame.time.set_timer(INC_SPEED, 1000)
+
+# Game Loop
+while True:
+    for event in pygame.event.get():
+        if event.type == INC_SPEED:
+            SPEED += 0.5
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    DISPLAYSURF.blit(background, (0, 0))
+    scores = font_small.render(str(SCORE), True, WHITE)
+    DISPLAYSURF.blit(scores, (10, 10))
+
+    for entity in all_sprites:
+        entity.move()
+        DISPLAYSURF.blit(entity.image, entity.rect)
+
+    if pygame.sprite.spritecollideany(P1, enemies):
+        pygame.mixer.Sound('crash.wav').play()
+        time.sleep(1)
+
+        DISPLAYSURF.fill(BLACK)
+        DISPLAYSURF.blit(game_over, (30, 250))
+
+        pygame.display.update()
+        for entity in all_sprites:
+            entity.kill()
+        time.sleep(2)
+        pygame.quit()
+        sys.exit()
+
+    pygame.display.update()
+    FramePerSec.tick(FPS)
